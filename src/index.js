3d4a9888b2135aa3289b3a029cbeebf63ff00f8c
Севастьянов <idatah55.gmail.com>
2025-10-21 12:07:52 +0300

    add const port = process.env.PORT || 4000

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import getApp from './routes/index.js';
 
 const app = await getApp();
 
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
