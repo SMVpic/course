517b93629dafe620984b51b7cb4121a87df76701
Севастьянов <idatah55.gmail.com>
2025-10-21 12:47:12 +0300

    add const host = 0.0.0.0

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = await getApp();
 
 const port = process.env.PORT || 4000;
 
+const host = '0.0.0.0'
+
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
